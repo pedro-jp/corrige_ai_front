4e592d0ee9509ed3aa91153da9e51ad66898028e
pedro-jp <joaopedroc035@gmail.com>
2025-01-18 23:29:33 -0300

    feat: add env

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,30 @@ import './App.css';
 import axios from 'axios';
 import { TiDocumentText } from 'react-icons/ti';
 import LoadingIcons from 'react-loading-icons';
+import { GiArtificialIntelligence } from 'react-icons/gi';
+
+type Line = {
+  LineText: string;
+};
+
+interface RedacaoFeedback {
+  nota: number;
+  pontos_fortes: string[];
+  pontos_a_melhorar: string[];
+  sugestoes: string[];
+  repertorio_possivel: string[];
+}
 
 function App() {
   const [image, setImage] = useState<File | null>(null);
   const [, setData] = useState<string>('');
-  const [isLoading, setIsLoading] = useState(false); // Bloqueio de requisições
+  const [isLoading, setIsLoading] = useState(false);
   const [lineCount, setLineCount] = useState<number>(0);
   const [displayedText, setDisplayedText] = useState<string>('');
+  const [feedback, setRedacaoFeedback] = useState<RedacaoFeedback>();
   const contentEditableRef = useRef<HTMLDivElement>(null);
   const [fontSize, setFontSize] = useState(16);
+  const [sendedText, setSendedText] = useState('');
   const formData = new FormData();
 
   if (image) {
@@ -24,7 +39,7 @@ function App() {
 
   const handleSendImage = async () => {
     if (image && !isLoading) {
-      setIsLoading(true); // Bloqueia novas requisições
+      setIsLoading(true);
       try {
         const response = await axios.post(
           'https://api.ocr.space/parse/image',
@@ -40,20 +55,31 @@ function App() {
         const parsedResults = response.data.ParsedResults[0];
         const lines = parsedResults.TextOverlay.Lines;
 
-        const parsedText = lines.map((line: any) => line.LineText).join('\n'); //eslint-disable-line
+        const parsedText = lines.map((line: Line) => line.LineText).join('\n');
         setData(parsedText);
         setLineCount(0);
         typeText(lines);
       } catch (error) {
         console.error(error);
       } finally {
-        setIsLoading(false); // Libera novas requisições
+        setIsLoading(false);
       }
     }
   };
 
-  type Line = {
-    LineText: string;
+  const handleAi = async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.post(`${process.env.BACKEND}/ai`, {
+        data: sendedText
+      });
+      setRedacaoFeedback(res.data);
+      console.log(feedback);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const typeText = (lines: Line[]) => {
@@ -72,6 +98,9 @@ function App() {
         setDisplayedText((prev) => prev + lineText + '<br />');
         index++;
         setLineCount(index);
+        setSendedText(
+          (prev) => prev + lineText.replace(/<br \/>/g, '\n') + '\n '
+        );
       } else {
         clearInterval(interval);
         setIsLoading(false);
@@ -92,7 +121,7 @@ function App() {
     if (image) {
       handleSendImage();
     }
-  }, [image]);
+  }, [image]); //eslint-disable-line
 
   useEffect(() => {
     const textContainer = contentEditableRef.current;
@@ -123,13 +152,15 @@ function App() {
             </div>
           </div>
           <div>
-            {displayedText && (
+            {sendedText && (
               <button
                 style={{ cursor: btn }}
                 onClick={() => {
                   setImage(null);
                   setDisplayedText('');
                   setLineCount(0);
+                  setSendedText('');
+                  contentEditableRef.current!.innerText = '';
                 }}
               >
                 Limpar
@@ -155,12 +186,61 @@ function App() {
           ref={contentEditableRef}
           style={{ fontSize: `${fontSize}px` }}
           contentEditable
+          onInput={() => {
+            setSendedText(contentEditableRef.current?.innerText || '');
+          }}
           dangerouslySetInnerHTML={{ __html: displayedText }}
         />
         <div className='footer'>
+          {sendedText && contentEditableRef.current?.innerText !== '' && (
+            <button
+              type='button'
+              style={{ cursor: btn }}
+              disabled={isLoading}
+              onClick={handleAi}
+            >
+              {!isLoading ? (
+                <GiArtificialIntelligence size={25} />
+              ) : (
+                <LoadingIcons.Grid color='white' height={20} width={20} />
+              )}
+            </button>
+          )}
           <p>{lineCount > 0 && `Linhas ${lineCount}`}</p>
         </div>
       </div>
+      {feedback && (
+        <div className='container'>
+          <div className='response_container'>
+            <h2>Nota {feedback.nota}</h2>
+            <div>
+              <strong>Pontos fortes:</strong>
+              {feedback.pontos_fortes.map((ponto, index) => (
+                <p key={index}>{ponto}</p>
+              ))}
+            </div>
+            <div>
+              <strong>Pontos a melhorar:</strong>
+              {feedback.pontos_a_melhorar.map((ponto, index) => (
+                <p key={index}>{ponto}</p>
+              ))}
+            </div>
+            <div>
+              <strong>Sugestões:</strong>
+              {feedback.sugestoes.map((obs, index) => (
+                <p key={index}>{obs}</p>
+              ))}
+            </div>
+
+            <div>
+              <strong>Repertório possivel:</strong>
+              {feedback.repertorio_possivel.map((obs, index) => (
+                <p key={index}>{obs}</p>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
     </main>
   );
 }
